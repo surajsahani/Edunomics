7907abe7936fa7526f18a3819e9f6e28f48fcb42
Suraj sahani <surajkumarsahani1997@gmail.com>
2020-06-25 21:48:14 +0530

    Edunomics App

---
+++ app/src/main/java/com/example/edunomics/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.edunomics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Handler handler;
+    ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.img);
+        img.animate().alpha(4000).setDuration(0);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+    }
+}
+
+
+
